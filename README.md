92052d45dfe65276f1ac029a7cc62b6c422b312e
Fadoon T <fadoon.tar@gmail.com>
2024-03-27 18:46:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Car Ordering App 
+
+## Features
+1. Customers can request rides 
+2. Customers can see their rides accepted in real-time
+3. Drivers can see customers requesting a ride
+4. Customer's ride request expires if no driver is available within a minute
+5. When a driver accepts a ride, another driver cannot accept same ride
+
+## Dependencies
+1. Express
+2. Socket.io
+
+## Getting Started
+
+Clone the repo   
+
+```
+git clone https://github.com/Fa-doon/Car-ordering-app.git
+
+```
+
+Install the Dependencies  
+
+```
+npm install OR yarn install
+
+```
+
+
+
+
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const socketIO = require("socket.io");
+const OrderingApp = require("./orderingApp")
+
+const app = express();
+const PORT = 4001;
+const server = http.createServer(app);
+// So server can function as a websocket
+const io = socketIO(server);
+
+app.get("/", (req, res) =>{
+  res.send("Car Ordering App")
+})
+
+app.get("/sender", (req, res) =>{
+  res.sendFile(path.join(__dirname, "sender.html"))
+})
+
+app.get("/driver", (req, res) =>{
+  res.sendFile(path.join(__dirname, "driver.html"))
+})
+
+
+const orderingApp = new OrderingApp()
+
+// io to listen to connection/events
+io.on("connection", (socket) => {
+    console.log("User has connected: ", socket.id);
+
+    socket.on("join", (user_type, Username )=>{
+      const userInfo = {
+          socket:socket,
+          user_type:user_type,
+          name:Username
+      }
+
+      orderingApp.joinSession(userInfo)
+  })
+
+    socket.on("requestOrder", (order) =>{
+      orderingApp.requestOrder(order)
+    })
+
+    socket.on("acceptOrder", (id, driverId)=>{
+      orderingApp.acceptOrder(id, driverId)
+    })
+
+    socket.on("rejectOrder", (id, driverId) =>{
+      orderingApp.rejectOrder(id, driverId)
+    })
+
+    socket.on("finishRide", (id, driverId) =>{
+      orderingApp.finishRide(id, driverId)
+    })
+
+});
+
+server.listen(PORT, () => {
+  console.log(`server is listening on port http://localhost:${PORT}`);
+});
